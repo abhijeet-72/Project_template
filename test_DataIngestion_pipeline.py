3065ed72c35c76a27e7ef33b70179a99aed03789
Abhijeet <aj.datsc72@gmail.com>
2023-03-29 22:46:22 +0530

    Data Ingestion Completed; raw, test & train dataset exported

---
+++ test_DataIngestion_pipeline.py
@@ -0,0 +1,14 @@
+from visa.pipeline.pipeline import Pipeline
+from visa.logger import logging
+
+def main():
+    try:
+        pipeline = Pipeline()
+        pipeline.run_pipeline()
+
+    except Exception as e:
+            logging.error(f"{e}")
+            print(e)
+
+if __name__ == "__main__":
+     main()
