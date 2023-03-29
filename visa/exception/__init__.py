824bb0ff89d1316e72af820a2c9ba8ad9a20a374
Abhijeet <aj.datsc72@gmail.com>
2023-03-29 20:18:59 +0530

    Logging & Exception

---
+++ visa/exception/__init__.py
@@ -0,0 +1,31 @@
+import sys
+
+class CustomException(Exception):
+    
+    def __init__(self, error_message:Exception, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = CustomException.get_detailed_error_message(error_message = error_message,
+                                                                        error_detail = error_detail)
+
+    @staticmethod
+    def get_detailed_error_message(error_message:Exception, error_detail:sys) -> str:
+        
+        _,_ ,exc_tb = error_detail.exc_info()
+        
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        try_block_line_number = exc_tb.tb_lineno
+        exception_block_line_number = exc_tb.tb_frame.f_lineno
+        
+        error_message = f'''
+        Error occured in script: [ {file_name} ] at 
+        try block line number: [{try_block_line_number}] and 
+        exception block line number: [{exception_block_line_number}] 
+        Error Message: [{error_message}]
+        '''
+        return error_message
+
+    def __str__(self):
+        return self.error_message
+
+    def __repr__(self) -> str:
+        return CustomException.__name__.str()
